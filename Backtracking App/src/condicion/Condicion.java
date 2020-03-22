cb02625e5906d707e06495535a53694d85441d3e
Martin Visca <martinivisca@gmail.com>
2020-03-22 19:23:39 -0300

    Agregada la clase Condicion.

--- Backtracking App/src/condicion/Condicion.java
+++ Backtracking App/src/condicion/Condicion.java
@@ -3,7 +3,7 @@ package condicion;
 import estado.Estado;
 
 public interface Condicion {
-    /*Interfaz que se usará en las heurísticas encargadas de la poda de estados de la ejecución del backtracking.*/
+    /**Interfaz que se usará en las heurísticas encargadas de la poda de estados de la ejecución del backtracking.**/
 
     public boolean cumpleCondicion(Estado e);
 }
