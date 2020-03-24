13a4feacc3728e8ef112000284bb3bbf3acffe41
Martin Visca <martinivisca@gmail.com>
2020-03-24 19:29:55 -0300

    Agregado a EjecutorBack los métodos de backtracking predefinido y personalizado.

--- Backtracking App/src/ejecutorBack/Ejecutor.java
+++ Backtracking App/src/ejecutorBack/Ejecutor.java
@@ -1,7 +1,10 @@
 package ejecutorBack;
 
+import accion.Accion;
 import condicion.Condicion;
 import estado.Estado;
+
+import java.util.Vector;
 
 public abstract class Ejecutor {
     /**Clase encargada de llevar a cabo la ejecucion del backtracking.**/
@@ -9,6 +12,7 @@ public abstract class Ejecutor {
     private Condicion hoja;
     private Condicion poda;
     private Estado solucion;
+    private Accion accion;
 
     public void setHoja(Condicion hoja) {
         this.hoja = hoja;
@@ -34,7 +38,35 @@ public abstract class Ejecutor {
         return solucion;
     }
 
-    public abstract void ejecutarBacktracking(Estado inicial, Estado solucion);
+    public Accion getAccion() {
+        return accion;
+    }
+
+    public void setAccion(Accion accion) {
+        this.accion = accion;
+    }
+
+    public void backtrackingEsquemaGeneral(Estado e, Estado solucion){
+        //Método que realiza el backtracking en su forma más simple.
+        if (!this.hoja.cumpleCondicion(e))
+            this.accion.realizarAccion(e.getEstructura());
+        else{
+            int nroHijo = 0;
+            Vector <Estado> auxiliar = e.getEstadosSiguientes();
+
+            while (!auxiliar.isEmpty()){ //
+                Estado siguiente = auxiliar.firstElement();
+                auxiliar.removeElementAt(0);
+
+                if (!poda.cumpleCondicion(e))
+                    this.backtrackingEsquemaGeneral(siguiente, solucion);
+                nroHijo++;
+            }
+        }
+    }
+
+    public abstract void backtrackingPersonalizado(Estado e, Object ... parametros);
+    //Método que da la posibilidad de definir un propio backtracking con las herramientas brindadas.
 
     public abstract Estado calcularSolucion();
 }
--- Backtracking App/src/impresor/Impresor.java
+++ Backtracking App/src/impresor/Impresor.java
@@ -3,6 +3,7 @@ package impresor;
 import estructura.Estructura;
 
 public interface Impresor {
+    /**Interfaz que se ocupa del comportamiento del programa en la impresión. **/
 
     public void imprimir(Estructura e);
 }
