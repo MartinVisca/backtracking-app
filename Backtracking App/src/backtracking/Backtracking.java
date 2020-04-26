62aa03d89f287ac38431fbba27038902f88ceb86
Martin Visca <martinivisca@gmail.com>
2020-04-26 15:22:44 -0300

    Agregada la clase Backtracking.

---
+++ Backtracking App/src/backtracking/Backtracking.java
@@ -0,0 +1,24 @@
+package backtracking;
+
+import estado.Estado;
+import poda.Poda;
+
+public class Backtracking {
+
+    private Poda poda;
+
+    public Backtracking(Poda poda) {
+        this.poda = poda;
+    }
+
+    public void iniciar(Estado inicial) {
+        //Acá también se inicia la interfaz.
+        inicial.backtracking(this.poda);
+    }
+
+    public void avanzar(Estado siguiente) {
+        //Con la interfaz ya iniciada, va mostrando los estados que resultan de cada ejecución del algoritmo.
+        siguiente.backtracking(this.poda);
+    }
+
+}
